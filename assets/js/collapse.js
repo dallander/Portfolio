b6440c307631e036c28548d1654dcfae4caadb81
Dalander <bigdatahi7@gmail.com>
2025-06-28 00:50:56 -0300

    Alterado HTML CSS e foi refeito a lógica de abrir e fechar a pasta

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -1,23 +1,36 @@
 
 
 
-document.querySelectorAll(".imgpasta").forEach(function (pastas,indice){
-    pastas.addEventListener("click", ()=>{
-        let alvo = document.querySelectorAll(".conteudoCollapse")[indice];
-        if(alvo.classList.contains("escondido")){
-            alvo.classList.remove("escondido");
-            alvo.classList.add("mostraCollapse");
-        }else{
-            alvo.classList.remove("mostraCollapse")
-            alvo.classList.add("escondido")
-        }
 
-        console.log(alvo)
-    })
-    
+
+document.addEventListener("click", function(elemento){
+    const pasta = elemento.target.closest(".imgpasta");
+        if(pasta){
+            //SELECIONA O MEU COLAPSE ESCONDIDO BASEADO NA PASTA QUE EU CLIQUEI
+             const pastaAtual =pasta.closest(".containerCollapse");
+             //AGORA EU SELECIONO A TAG QUE TEM A CLASSE QUE EU QUERO FAZER A ALTERAÇÃO
+             const conteudo = pastaAtual.querySelector(".conteudoCollapse");
+            
+             //REMOVE A CLASSE SE TIVER E SE NÃO TIVER ADICIONA
+             
+             if(conteudo.classList.contains("escondido")){
+                conteudo.classList.remove("escondido");
+                //CHAMA FUNÇÃO PARA ABRIR PASTA
+                abrirFecharPasta(pasta,true)
+             }else{
+                conteudo.classList.add("escondido");
+                //CHAMA FUNÇÃO PARA FECHAR A PASTA
+                abrirFecharPasta(pasta,false);
+             };
+
+        };
+
 });
 
-
+function abrirFecharPasta(pasta, aberta){
+pasta.src = aberta ?  
+"/assets/images/pastas-mural/pasta-aberta.png" : "/assets/images/pastas-mural/pasta-fechada.png";
+};
 
 
 
